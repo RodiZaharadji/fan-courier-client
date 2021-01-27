091f7fff4277d1240612ce9d8ba92174abe7cf3c
zaharadji.rodislav <zaharadji2002@gmail.com>
2021-01-27 09:14:19 +0200

    Fix pass

--- fan_courier_client/common.py
+++ fan_courier_client/common.py
@@ -2,12 +2,12 @@ class BaseObject:
     def __init__(self, **kwargs):
         self.client_id = kwargs.pop('client_id', '7032158')
         self.username = kwargs.pop('username', 'clienttest')
-        self.password = kwargs.pop('password', 'testing')
+        self.password = kwargs.pop('user_pass', 'testing')
 
     @property
     def params(self):
         return {
             'username': self.username,
-            'user_pass': self.password,
+              'user_pass': self.password,
             'client_id': self.client_id,
         }
